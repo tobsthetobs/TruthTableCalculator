866553b4ebff159d69213f68b8483d4dd560d950
tobsthetobs <73800087+tobsthetobs@users.noreply.github.com>
2023-12-16 00:06:22 +0100

    Commit
    2

--- GUI.py
+++ GUI.py
@@ -3,12 +3,24 @@ import tkinter as tk
 from string import ascii_uppercase
     
 class Table_Object(ck.CTkFrame):
-    def __init__(self, master, shape, table_data):
+    def __init__(self, master, terms, cols, table_data):
         super().__init__(master)
         self.table_data = table_data
         
-        for row in shape[0]:
-            for cols in shape[1]:
+        for i in range(cols):
+            label = ck.CTkLabel(self, text=ascii_uppercase[i])
+            label.grid(row = 0, column = i, padx=5, pady=5)
+        
+        self.table_elements = []
+        for row in range(terms):
+            for col in range(cols):
+                entry = ck.CTkEntry(self)
+                entry.insert(0, self.table_data[row][col])
+                entry.grid(row = row+1, column=col, padx=5, pady=5)
+                self.table_elements.append(entry)
+        
+    def get_table_data():
+        pass
                 
         
  
@@ -17,11 +29,13 @@ class App(ck.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("900x900")
-        self.table_dims = [0,0]
-        self.table = None
+        
+        self.table_data = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        # self.table = Table_Object(self, 4,2,self.table_data)
+        # self.table.pack(padx = 20, pady = 20, fill=ck.BOTH, expand = True)
         
         self.add_table_button = ck.CTkButton(self, text="Add Table", command=self.tablebutton)
-        self.add_table_button.grid(row = 0)
+        self.add_table_button.pack(pady = 10)
         
         
     def tablebutton(self):
@@ -34,10 +48,22 @@ class App(ck.CTk):
         
         # print for debugging.
         print(terms,cols)
-        self.
-    
-    def update_table(self):
+        
+        # Make table and assign all initial values to zero for data. 
+        self.table_data = 0
+        for n in [cols, terms]:
+            self.table_data = [self.table_data] * n
+        print(self.table_data)
+        for row in range(terms):
+            for col in range(cols):
+                self.table_data[row][col] = 0
+                
+        self.table = Table_Object(self,terms, cols, self.table_data)
+        self.table.pack(padx = 20, pady = 20, fill=ck.BOTH, expand = True)
+        
+    def calculate_kmap(self):
         pass
+    
 
 if __name__ == "__main__":
     app = App()
